13fafc06d019a728e9083840cef51ef9665fa1da
Aima Khan <aimak748@gmail.com>
2023-06-26 09:31:22 +0100

    completed with login and sign up form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function Login() {
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    console.log('Email:', email);
+    console.log('Password:', password);
+  }, [email, password]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Submitted');
+
+    setEmail('');
+    setPassword('');
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="login template d-flex justify-content-center align-items-center p-5">
+      <div className="p-5 rounded" style={{ width: '400px' }}> {/* Inline CSS for width */}
+        <form onSubmit={handleSubmit}>
+          <h3 style={{ color: '#001A33' }}>LOGIN IN</h3>
+          <div className="mb-3">
+            <label htmlFor="email" style={{ color: '#001A33' }}>
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Your Password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-2">
+            <input type="checkbox" className="custom-control custom-checkbox" name="" id="checkbox" />
+            <label htmlFor="checkbox" className="custom-input-label">
+              Remember me
+            </label>
+          </div>
+          <div className="d-grid">
+            <button className="btn btn-danger" type="submit">
+              Sign In
+            </button>
+          </div>
+          <p>
+            Forgot <a href="">Password?</a> <br />
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default Login;
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,7 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function SignUp() {
+function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  useEffect(() => {
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Phone Number:', phoneNumber);
+  }, [name, email, password, phoneNumber]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submitted');
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="signup template d-flex justify-content-center align-items-center p-5">
+      <div className="signup-form-wrapper">
+        <form onSubmit={handleSubmit} className="p-5">
+          <h3 style={{ color: '#001A33' }}>SIGN UP</h3>
+          <div className="mb-3">
+            <label htmlFor="name" style={{ color: '#001A33' }}>
+              Name
+            </label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" style={{ color: '#001A33' }}>
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Your Password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input
+              type="tel"
+              placeholder="Your Phone Number"
+              className="form-control"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+          
+          <div className="d-grid">
+            <button className="btn btn-primary">Sign Up</button>
+          </div>
+        </form>
+      </div>
+      <div >
+        <img src="https://cdni.iconscout.com/illustration/premium/thumb/ecommerce-website-3613833-3025953.png" alt="Sign Up Illustration" />
+      </div>
+    </div>
+  );
 }
+
+export default SignUp;
+
+
+
