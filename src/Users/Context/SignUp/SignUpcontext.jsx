32cb960898f9c84ea7b5e48ce2d90e06ba3c4c1f
melodyBee <abeerafatimarub@gmail.com>
2023-08-08 02:35:24 +0500

    localstorage worked but idk how and why...

--- src/Users/Context/SignUp/SignUpcontext.jsx
+++ src/Users/Context/SignUp/SignUpcontext.jsx
@@ -1,27 +1,30 @@
-import React from "react";
-import { useReducer, createContext, useEffect } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { reducer } from "./SignUpreducer";
 
 const getUserData = () => {
   let userData = localStorage.getItem("user");
-  if (userData == {}) {
+  if (userData === "undefined") {
+    // Compare with the string "undefined"
     return {};
   } else {
     return JSON.parse(userData);
   }
 };
+
 const initialData = {
   user: getUserData(),
 };
 
-export const GlobalContext = createContext("Initial Value");
+export const GlobalContext = createContext();
+
 export default function ContextProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialData);
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(state.cart));
-    console.log("I am running");
+    localStorage.setItem("user", JSON.stringify(state.user));
+    console.log("I'm running");
   }, [state.user]);
+
   return (
     <GlobalContext.Provider value={{ state, dispatch }}>
       {children}
